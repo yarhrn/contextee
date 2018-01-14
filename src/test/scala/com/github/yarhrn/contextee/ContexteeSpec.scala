5f7ddcbb34f608a7718012448ee8a655bd7a9ece
yaroslavh <yaroslavh@wix.com>
2018-01-14 14:47:56 +0200

    update filename add typealias

--- src/test/scala/com/github/yarhrn/contextee/ContexteeSpec.scala
+++ src/test/scala/com/github/yarhrn/contextee/ContexteeSpec.scala
@@ -2,8 +2,8 @@ package com.github.yarhrn.contextee
 
 import cats.Monad
 import cats.data.Reader
-import com.github.yarhrn.contextee.Contextee
 import org.scalatest._
+import scala.language.higherKinds
 
 class ContexteeSpec extends FlatSpec with Matchers {
   "Contextee" should "be implicitly summoned for reader" in {
@@ -11,16 +11,17 @@ class ContexteeSpec extends FlatSpec with Matchers {
       def get(key: String): F[String]
     }
     case class Ctx(key: String)
+    type Context[F[_]] = Contextee[F, Ctx]
 
     import cats.implicits._
-    def program[F[_] : Monad : KStore : Lambda[F[_] => Contextee[F, Ctx]]]: F[String] = for {
+    def program[F[_] : Monad : KStore : Context]: F[String] = for {
       ctx <- Contextee[F, Ctx].context
       value <- implicitly[KStore[F]].get(ctx.key)
     } yield value
 
     type MonadStack[A] = Reader[Ctx, A]
 
-    implicit def kstore[F[_]:Monad] = new KStore[F] {
+    implicit def kstore[F[_] : Monad] = new KStore[F] {
       override def get(key: String): F[String] = Monad[F].pure(key + key)
     }
 
