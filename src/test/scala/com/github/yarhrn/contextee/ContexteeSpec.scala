0606924099635f290e6ea100e4c70ec28e6b8546
yaroslavh <yaroslavh@wix.com>
2018-01-15 15:53:46 +0200

    remove code duplication in tests

--- src/test/scala/com/github/yarhrn/contextee/ContexteeSpec.scala
+++ src/test/scala/com/github/yarhrn/contextee/ContexteeSpec.scala
@@ -11,62 +11,50 @@ class ContexteeSpec extends FreeSpec with Matchers {
   "Contextee should" - {
 
     "be implicitly summoned for reader" in {
-      trait KStore[F[_]] {
-        def get(key: String): F[String]
-      }
-      case class Ctx(key: String)
-      type Context[F[_]] = Contextee[F, Ctx]
-
-      import cats.implicits._
-      def program[F[_] : Monad : KStore : Context]: F[String] = for {
-        ctx <- Contextee[F, Ctx].context
-        value <- implicitly[KStore[F]].get(ctx.key)
-      } yield value
-
-      type MonadStack[A] = Reader[Ctx, A]
-
-      implicit def kstore[F[_] : Monad] = new KStore[F] {
-        override def get(key: String): F[String] = Monad[F].pure(key + key)
-      }
-
+      type MonadStack[A] = Reader[ContextHolder, A]
       import Contextee._
-      program[MonadStack].run(Ctx("jk")) should be("jkjk")
+      program[MonadStack].run(ContextHolder("jk")) should be("jkjk")
     }
 
     "be implicitly summoned for state" in {
-      trait KStore[F[_]] {
-        def get(key: String): F[String]
-      }
-      case class Ctx(key: String)
-      type Context[F[_]] = Contextee[F, Ctx]
-
-      import cats.implicits._
-      def program[F[_] : Monad : KStore : Context]: F[String] = for {
-        ctx <- Contextee[F, Ctx].context
-        value <- implicitly[KStore[F]].get(ctx.key)
-      } yield value
-
-      type MonadStack[A] = State[Ctx, A]
-
-      implicit def kstore[F[_] : Monad] = new KStore[F] {
-        override def get(key: String): F[String] = Monad[F].pure(key + key)
-      }
-
+      type MonadStack[A] = State[ContextHolder, A]
       import Contextee._
-      program[MonadStack].run(Ctx("jk")).value._2 should be("jkjk")
+      program[MonadStack].run(ContextHolder("jk")).value._2 should be("jkjk")
     }
 
     "be usable in free application" in {
       import ContexteeFree._
 
-      case class Ctx(s: String)
       type Program[A] = Free[ContexteeAlgebra, A]
-      implicit val ctx = Ctx("asd")
-      val a: Contextee[Program, Ctx] = implicitly[Contextee[Program, Ctx]]
+      implicit val context = ContextHolder("asd")
+      val contextee: Contextee[Program, ContextHolder] = implicitly[Contextee[Program, ContextHolder]]
 
-      a.context.map(_.s * 2).foldMap(new ContextFreeInterpreter[Id]) should be("asdasd")
+      val program = contextee.context.map(_.key * 2)
+
+      program.foldMap(new ContextFreeInterpreter[Id]) should be("asdasd")
 
     }
   }
 
+  case class ContextHolder(key: String)
+
+  def program[F[_] : Monad : Lambda[F[_] => Contextee[F, ContextHolder]]]: F[String] = {
+    trait KStore[F[_]] {
+      def get(key: String): F[String]
+    }
+
+    type Context[F[_]] = Contextee[F, ContextHolder]
+
+    import cats.implicits._
+    def program[F[_] : Monad : KStore : Context]: F[String] = for {
+      ctx <- Contextee[F, ContextHolder].context
+      value <- implicitly[KStore[F]].get(ctx.key)
+    } yield value
+
+    implicit def kstore[F[_] : Monad] = new KStore[F] {
+      override def get(key: String): F[String] = Monad[F].pure(key + key)
+    }
+
+    program[F]
+  }
 }
